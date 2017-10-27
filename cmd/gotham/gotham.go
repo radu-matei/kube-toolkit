f7d8a3ee66debdb6a00f82095074db460657b2c9
radu-matei <matei.radu94@gmail.com>
2017-10-27 12:43:44 +0300

    Add initial working server
    Signed-off-by: radu-matei <matei.radu94@gmail.com>

---
+++ cmd/gotham/gotham.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"io"
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var (
+	globalUsage = "Gotham - the cloud service deployment server for Kubernetes"
+
+	flagDebug bool
+)
+
+func newRootCmd(out io.Writer, in io.Reader) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:          "gotham",
+		Short:        globalUsage,
+		Long:         globalUsage,
+		SilenceUsage: true,
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			if flagDebug {
+				log.SetLevel(log.DebugLevel)
+			}
+		},
+	}
+
+	cmd.AddCommand(newStartCmd(out))
+	return cmd
+}
+
+func main() {
+	cmd := newRootCmd(os.Stdout, os.Stdin)
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
---
+++ cmd/gotham/start.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"io"
+
+	"github.com/radu-matei/joker/pkg/gotham"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	startUsage = "starts the Gotham server"
+
+	listenAddress string
+)
+
+type startCmd struct {
+	out           io.Writer
+	listenAddress string
+}
+
+func newStartCmd(out io.Writer) *cobra.Command {
+	startCmd := &startCmd{}
+
+	cmd := &cobra.Command{
+		Use:   "start",
+		Short: startUsage,
+		Long:  startUsage,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return startCmd.run()
+		},
+	}
+
+	flags := cmd.Flags()
+	flags.StringVar(&listenAddress, "listen-addr", "0.0.0.0:10000", "the Gotham server listen address")
+
+	return cmd
+}
+
+func (cmd *startCmd) run() error {
+
+	cfg := &gotham.ServerConfig{
+		ListenAddress: listenAddress,
+	}
+
+	return gotham.NewServer(cfg).Serve()
+}
