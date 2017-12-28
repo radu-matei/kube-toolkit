4637c68327a54e15857bc24ca85dbc4484749bd3
Radu Matei <matei.radu94@gmail.com>
2017-12-28 21:22:30 +0200

    Add initial gateway

---
+++ cmd/gateway/gateway.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	gw "github.com/radu-matei/kube-toolkit/pkg/rpc"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+var (
+	echoEndpoint = flag.String("backend", "localhost:10000", "endpoint of ktkd server")
+)
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := gw.RegisterKTKHandlerFromEndpoint(ctx, mux, *echoEndpoint, opts)
+	if err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(":8080", mux)
+}
+
+func main() {
+	flag.Parse()
+	defer glog.Flush()
+
+	if err := run(); err != nil {
+		glog.Fatal(err)
+	}
+}
